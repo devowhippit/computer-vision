874137a5be372452ae9a8b177e5418687e679f0a
devowhippit <devon@expdesign.io>
2019-03-12 21:53:17 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,169 @@
+import * as posenet from '@tensorflow-models/posenet';
+import {drawBoundingBox, drawKeypoints, drawSkeleton} from './util';
+
+const videoWidth = 600;
+const videoHeight = 500;
+const isAndroid = () => /Android/i.test(navigator.userAgent);
+const isiOS = () => /iPhone|iPad|iPod/i.test(navigator.userAgent);
+const isMobile = () => isAndroid() || isiOS();
+
+const guiState = {
+  algorithm: 'single-pose',
+  input: {
+    mobileNetArchitecture: isMobile() ? '0.50' : '0.75',
+    outputStride: 16,
+    imageScaleFactor: 0.5,
+  },
+  singlePoseDetection: {
+    minPoseConfidence: 0.1,
+    minPartConfidence: 0.5,
+  },
+  multiPoseDetection: {
+    maxPoseDetections: 5,
+    minPoseConfidence: 0.15,
+    minPartConfidence: 0.1,
+    nmsRadius: 30.0,
+  },
+  output: {
+    showVideo: true,
+    showSkeleton: true,
+    showPoints: true,
+    showBoundingBox: false,
+  },
+  net: null,
+};
+
+async function setupCamera() {
+  if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+    throw new Error(
+        'Browser API navigator.mediaDevices.getUserMedia not available');
+  }
+
+  const video = document.getElementById('video');
+
+  video.width = videoWidth;
+  video.height = videoHeight;
+
+  const mobile = isMobile();
+  
+  const stream = await navigator.mediaDevices.getUserMedia({
+    'audio': false,
+    'video': {
+      facingMode: 'user',
+      width: mobile ? undefined : videoWidth,
+      height: mobile ? undefined : videoHeight,
+    },
+  });
+
+  video.srcObject = stream;
+
+  return new Promise((resolve) => {
+    video.onloadedmetadata = () => {
+      resolve(video);
+    };
+  });
+}
+
+async function loadVideo() {
+  const video = await setupCamera();
+
+  video.play();
+
+  return video;
+}
+
+/**
+ * Feeds an image to posenet to estimate poses - this is where the magic
+ * happens. This function loops with a requestAnimationFrame method.
+ */
+function detectPoseInRealTime(video) {
+  const canvas = document.getElementById('output');
+  const ctx = canvas.getContext('2d');
+  // since images are being fed from a webcam
+  const flipHorizontal = true;
+
+  canvas.width = videoWidth;
+  canvas.height = videoHeight;
+
+  async function poseDetectionFrame() {
+    if (guiState.changeToArchitecture) {
+      // Important to purge variables and free up GPU memory
+      guiState.net.dispose();
+
+      // Load the PoseNet model weights for either the 0.50, 0.75, 1.00, or 1.01
+      // version
+      guiState.net = await posenet.load(+guiState.changeToArchitecture);
+
+      guiState.changeToArchitecture = null;
+    }
+
+    // Scale an image down to a certain factor. Too large of an image will slow
+    // down the GPU
+    const imageScaleFactor = guiState.input.imageScaleFactor;
+    const outputStride = +guiState.input.outputStride;
+
+    let poses = [];
+    let minPoseConfidence;
+    let minPartConfidence;
+
+    const pose = await guiState.net.estimateSinglePose(
+      video, imageScaleFactor, flipHorizontal, outputStride);
+
+    poses.push(pose);
+
+    minPoseConfidence = +guiState.singlePoseDetection.minPoseConfidence;
+    minPartConfidence = +guiState.singlePoseDetection.minPartConfidence;
+
+    ctx.clearRect(0, 0, videoWidth, videoHeight);
+
+    if (guiState.output.showVideo) {
+      ctx.save();
+      ctx.scale(-1, 1);
+      ctx.translate(-videoWidth, 0);
+      ctx.drawImage(video, 0, 0, videoWidth, videoHeight);
+      ctx.restore();
+    }
+
+    // For each pose (i.e. person) detected in an image, loop through the poses
+    // and draw the resulting skeleton and keypoints if over certain confidence
+    // scores
+    poses.forEach(({score, keypoints}) => {
+      if (score >= minPoseConfidence) {
+        if (guiState.output.showPoints) {
+          drawKeypoints(keypoints, minPartConfidence, ctx);
+        }
+
+        if (guiState.output.showSkeleton) {
+          drawSkeleton(keypoints, minPartConfidence, ctx);
+        }
+
+        if (guiState.output.showBoundingBox) {
+          drawBoundingBox(keypoints, ctx);
+        }
+      }
+    });
+
+    requestAnimationFrame(poseDetectionFrame);
+  }
+
+  poseDetectionFrame();
+}
+
+/**
+ * Kicks off the demo by loading the posenet model, finding and loading
+ * available camera devices, and setting off the detectPoseInRealTime function.
+ */
+export async function bindPage() {
+  // Load the PoseNet model weights with architecture 0.75
+  const net = await posenet.load(0.75);
+  let video = await loadVideo();
+
+  guiState.net = net;
+
+  detectPoseInRealTime(video);
+}
+
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+// kick off the demo
+bindPage();
